4290b3c662c362ee5b8a3ff735e1499ea5799c0e
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-26 21:04:35 +0100

    used NSURLSession to parse JSON

--- webrequest-test/ViewController.swift
+++ webrequest-test/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://swapi.co/api/people/1"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        session.dataTaskWithURL(url) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+            
+            if let responseData = data {
+                
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
+                    print(json)
+                } catch {
+                    print("Could not serialize")
+                }
+            }
+        }.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
